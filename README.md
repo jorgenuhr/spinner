69497dea66d5855d5f8c999e271fd3d7b4f35dbe
Jörgen Uhr <>
2024-11-14 09:52:17 +0100

    fixed some stuff

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # spinner
 Bash function for animating a waiting effect for a background process
+
+## Usage
+See example file for how to use it in your bash script
+
+
---
+++ example.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# example how to use the spinner function
+#
+source spinner.func
+
+for (( i=1;i<9;i++)); do
+  spinner -t "Example spinner ${i}:" -c "sleep 2" -s ${i}
+done
+
+spinner -t "Example error:" -c "commandthatdoesntexist"
+
+echo -e "\n\n Done"
+
